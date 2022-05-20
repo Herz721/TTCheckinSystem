4b8ced0b8b43e7615b02b9265f50366ebc00bc95
Herz721 <haozheli@usc.edu>
2022-05-20 09:54:58 -0700

    add Scanner() class

--- scanner.py
+++ scanner.py
@@ -2,43 +2,38 @@ import scapy.all as scapy
 import time
 import subprocess
 
-
-NETWORK = "10.0.0.61/24"
-INTERVAL = 30  # seconds
-dictionary = {
-    '11:22:22:33:44:55': 'John Mobile',
-    '66:77:88:99:11:22': 'Eric PC',
-    '33:44:55:66:77:88': 'Bob Mobile'
-}
+class Scanner():
+    def __init__(self, network = "10.0.0.61/24", interval = 30):
+        self.network = network
+        self.interval = interval  # seconds
 
 
-def scan(ip):
-    macList = set()
-    arp_request = scapy.ARP(pdst=ip)
-    broadcast = scapy.Ether(dst='ff:ff:ff:ff:ff:ff')
-    arp_request_broadcast = broadcast/arp_request
-    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
-    # src = mac address; psrc = ip address; dst = local mac address; pdst = local ip address
-    for host in answered_list:
-        macList.add(host[1].src)
-    return macList
+    def scan(self):
+        macList = set()
+        arp_request = scapy.ARP(pdst = self.network)
+        broadcast = scapy.Ether(dst='ff:ff:ff:ff:ff:ff')
+        arp_request_broadcast = broadcast/arp_request
+        answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+        # src = mac address; psrc = ip address; dst = local mac address; pdst = local ip address
+        for host in answered_list:
+            macList.add(host[1].src)
+        return macList
 
-
-def connection_change(hosts, action):
-    if action not in ("connected", "disconnected"):
-        raise ValueError(f"Invalid action: {action}")
-    for host in hosts:
-        device = dictionary[host] if host in dictionary else 'unknown device'
-        if action == 'connected':
-            say = f"echo {device} connected | cscript C:\\Progra~1\\Jampal\\ptts.vbs"
-        else:
-            say = f"echo {device} disconnected | cscript C:\\Progra~1\\Jampal\\ptts.vbs"
-        subprocess.call(say, shell=True, stdout=None, stderr=None)
+    def connection_change(hosts, action):
+        if action not in ("connected", "disconnected"):
+            raise ValueError(f"Invalid action: {action}")
+        for host in hosts:
+            device = dictionary[host] if host in dictionary else 'unknown device'
+            if action == 'connected':
+                say = f"echo {device} connected | cscript C:\\Progra~1\\Jampal\\ptts.vbs"
+            else:
+                say = f"echo {device} disconnected | cscript C:\\Progra~1\\Jampal\\ptts.vbs"
+            subprocess.call(say, shell=True, stdout=None, stderr=None)
 
 
 def main():
-    old_macs = scan(NETWORK)
-    # print(len(old_macs))
+    scanner = Scanner()
+    old_macs = scanner.scan()
     for mac in old_macs:
         print(mac)
     # connection_change(old_macs, "connected")
