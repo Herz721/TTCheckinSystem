dba88397b3aa87f1fa400ec57ec0b054bcd3d5b6
Jay <jinghui@usc.edu>
2022-05-20 17:54:26 -0700

    dropdown list

--- InfoWeb/main.py
+++ InfoWeb/main.py
@@ -5,9 +5,11 @@ import os
 app = Flask(__name__, static_url_path='')
 COLUMNS = ['name', 'device', 'ip']
 
+
 @app.route('/')
 def init():
     return app.send_static_file("index.html")
+
 
 @app.route('/result')
 def result():
@@ -15,16 +17,19 @@ def result():
         df = pd.read_csv('localStorage.csv')
         print(df)
     else:
-        df = pd.DataFrame(columns = COLUMNS)
+        df = pd.DataFrame(columns=COLUMNS)
         print(df)
     name = request.args.get('name')
-    device = request.args.get('equipment')
+    device = request.args.get('device')
     ip = request.remote_addr
-    newData = pd.DataFrame({'name': name, 'device': device, 'ip': ip}, index=[1])
+    print(device)
+    newData = pd.DataFrame(
+        {'name': name, 'device': device, 'ip': ip}, index=[1])
     df = pd.concat([df, newData], ignore_index=True)
-    df.drop_duplicates(subset = ['ip'], keep = 'last', inplace = True)
-    df.to_csv('localStorage.csv', index = 0)
+    df.drop_duplicates(subset=['ip'], keep='last', inplace=True)
+    df.to_csv('localStorage.csv', index=0)
     return name
 
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8080, debug = True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
