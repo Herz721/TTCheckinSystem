95b94264b3dbcd467fc70e949b39dcd956796c7c
Jay <jinghui@usc.edu>
2022-06-02 09:57:31 -0700

    add response

--- InfoWeb/static/js/index.js
+++ InfoWeb/static/js/index.js
@@ -12,3 +12,20 @@ function submitInfo() {
     });
     return false;
 }
+
+function feedback() 
+    {
+        // Validate registeration
+        let name = document.querySelector("#name").value;
+        let device = document.querySelector("#device").value;
+        const devices = new Set(["phone", "pad", "pc", "other"]);
+        if (!name) {
+          alert("Missing Name");
+          return app.send_static_file("index.html");
+        } else if (!devices.has(device)) {
+          alert("Missing Device");
+          return app.send_static_file("index.html");
+        }
+        // Confirm registeration
+        alert("Hello, " + name + "! You've successfully registered! ");
+      }
