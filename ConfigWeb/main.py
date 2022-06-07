594f3a97a27a02e930ebf77fe7b18f7eb0271daa
Jay <jinghui@usc.edu>
2022-06-06 17:52:32 -0700

    login session

--- ConfigWeb/main.py
+++ ConfigWeb/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, session
+from flask import Flask, request, render_template, session, redirect
 from flask_sqlalchemy import SQLAlchemy
 import sys
 sys.path.append("../module")
@@ -13,6 +13,9 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:********@localhost/TrojanTech'
 db = SQLAlchemy(app)
 
+#config session
+app.config["SESSION_PERMANENT"] = False
+Session(app)
 
 hostname = socket.gethostname()
 ip = socket.gethostbyname(hostname)
@@ -20,16 +23,15 @@ ip = socket.gethostbyname(hostname)
 checkpoints = Checkpoints(db, ip)
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def init():
-    # TODO:Add login redirect
     if not session.get("name"):
         return redirect("/login")
     return render_template("configPage.html", config = checkpoints.config)
 
 @app.route("/login",methods = ["GET","POST"])
 def login():
-    if request.methods == "POST":
+    if request.method == "POST":
         session["name"] = request.form.get("name")
         return redirect("/")
     return render_template("login.html")
@@ -42,6 +44,13 @@ def result():
     checkpoints.scheduler.print_jobs()
     return render_template("configPage.html", config = checkpoints.config)
 
+@app.route("/logout")
+def logout():
+    session["name"] = None
+    return redirect("/")
 
 if __name__ == '__main__':
+    app.secret_key = 'super secret key'
+    app.config['SESSION_TYPE'] = 'filesystem'
+    app.debug = True
     app.run(host="0.0.0.0", port=9122, debug=False)
